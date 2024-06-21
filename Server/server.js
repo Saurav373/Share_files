747d2667dad73666edbd01f5be5d9e91ae8948e8
Saurav Kattel <sauravkattel777@gmail.com>
2024-06-21 19:21:06 +0545

    finished

--- Server/server.js
+++ Server/server.js
@@ -33,9 +33,7 @@ connectToMongoDB()
 deleteFiles(__dirname)
 
 
-app.get('/', (req, res) => {
-    res.send('Inshare Files Backend')
-})
+
 app.post('/upload', upload.single('file'), uploadFile)
 app.post('/fileinfo', FileInfo)
 app.get('/download/:uuid', DownloadFile)
