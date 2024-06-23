e808e06034e6dafda65b4e551439ba711683be97
Saurav Kattel <sauravkattel777@gmail.com>
2024-06-23 18:31:44 +0545

    deploy

--- Client/src/Components/FIle_input.jsx
+++ Client/src/Components/FIle_input.jsx
@@ -7,6 +7,7 @@ const File_input = () => {
     const [isDragging, setIsdragging] = useState(false);
     const [file, setfile] = useState(null)
     const InputFile = useRef();
+    const [loading, setLoading] = useState(false)
     const handleBrowse = () => {
         InputFile.current.click();
     };
@@ -25,65 +26,85 @@ const File_input = () => {
     const handleDrop = (e) => {
         e.preventDefault();
         setIsdragging(false);
-        const Files = e.dataTransfer.files;
-        if (Files.length === 1) {
-            InputFile.current.files = Files;
-        } else if (Files.length > 1) {
-            toast.error("Only one file allowted at a time");
+        try {
+            const Files = e.dataTransfer.files;
+            if (Files.length === 1) {
+                InputFile.current.files = Files;
+                handleSubmitData()
+            } else if (Files.length > 1) {
+                toast.error("Only one file allowted at a time");
+            }
+        } catch (err) {
+            console.log(err);
+            toast.error("Only a single file allowted");
         }
-        console.log(Files);
     };
     const handleSubmitData = async () => {
-        const form = new FormData();
-        form.append("file", InputFile.current.files[0]);
-        let res = await fetch("/upload", { method: "POST", body: form });
-        res = await res.json()
-        res.downloadLink = 'https://myinshare.netlify.app/download/' + res.downloadLink
-        setfile(res)
+        try {
+            const form = new FormData();
+            form.append("file", InputFile.current.files[0]);
+            setLoading(true)
+            let res = await fetch("https://inshare-3nn5.onrender.com/upload", { method: "POST", body: form });
+            res = await res.json()
+            res.downloadLink = 'https://myinshare.netlify.app/download/' + res.downloadLink
+            setfile(res)
+        } catch (err) {
+            toast.error("Only a single file allowted");
+        } finally {
+            setLoading(false)
+        }
     };
+
     return (
-        <div className="file-container" style={file && { height: '630px' }}>
-            <div
-                className={`upload-container ${isDragging && "dragging"}`}
-                onDragOver={handleDragEnter}
-                onDragLeave={handleDragLeave}
-                onDrop={handleDrop}
-            >
+        <>
 
-                <div className={`img-cont ${isDragging ? "dragged" : ""}`} >
-                    <img
-                        src="/file.svg"
-                        alt="file-icon"
-                        className="image center"
-                        draggable="false"
-                    />
-                    <img
-                        src="/file.svg"
-                        alt="file-icon"
-                        className="image left"
-                        draggable="false"
-                    />
-                    <img
-                        src="/file.svg"
-                        alt="file-icon"
-                        className="image right"
-                        draggable="false"
+            <div className="file-container" >
+                <div
+                    className={`upload-container ${isDragging && "dragging"}`}
+                    onDragOver={handleDragEnter}
+                    onDragLeave={handleDragLeave}
+                    onDrop={handleDrop}
+                >
+
+                    <div className={`img-cont ${(isDragging || loading) ? "dragged" : ""}`} >
+                        <img
+                            src="/file.svg"
+                            alt="file-icon"
+                            className="image center"
+                            draggable="false"
+                        />
+                        <img
+                            src="/file.svg"
+                            alt="file-icon"
+                            className="image left"
+                            draggable="false"
+                        />
+                        <img
+                            src="/file.svg"
+                            alt="file-icon"
+                            className="image right"
+                            draggable="false"
+                        />
+                    </div>
+                    <div className="text">
+                        {!loading && <>Drop Your Files here or , <span onClick={handleBrowse}>browse</span></>}
+
+                        {loading && 'Uploading...'}
+
+                    </div>
+                    <input
+                        type="file"
+                        id="file"
+                        ref={InputFile}
+                        onInput={handleSubmitData}
+                        name="file"
                     />
                 </div>
-                <div className="text">
-                    Drop Your Files here or , <span onClick={handleBrowse}>browse</span>
-                </div>
-                <input
-                    type="file"
-                    id="file"
-                    ref={InputFile}
-                    onInput={handleSubmitData}
-                    name="file"
-                />
+
+                {file && <SendEmail downloadlink={file.downloadLink} />}
             </div>
+        </>
 
-            {file && <SendEmail downloadlink={file.downloadLink} />}
-        </div>
     );
 };
 
