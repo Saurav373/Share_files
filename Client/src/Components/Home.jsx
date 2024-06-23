3ce5fa29d763bb6ae29da2adc4ef2edc56198405
Saurav373 <151314631+Saurav373@users.noreply.github.com>
2024-06-23 18:49:26 +0545

    Update Home.jsx

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -1,10 +1,8 @@
 import React, { Suspense, lazy } from 'react'
 import Loader from './Loader'
-<<<<<<< HEAD
 const inputFile = lazy(() => import('./inputFile'))
-=======
 const File_input = lazy(() => import('./FIle_input.'))
->>>>>>> 03233064bdd47dc25604cc0f09b283d81ee3e416
+
 
 const Home = () => {
   return (
